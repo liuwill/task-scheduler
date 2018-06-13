9711f5498f1f2ce223c2e34bf937a1fdb4ff24d9
liuwill <liuwei_will@qq.com>
2018-06-13 11:02:30 +0800

    fix redis config

--- bin/config/index.js
+++ bin/config/index.js
@@ -1,7 +1,12 @@
+const redisConfig = {
+  host: process.env.REDIS_HOST || 'localhost',
+  port: process.env.REDIS_PORT || 6379,
+}
+
+if (process.env.REDIS_PASSWORD) {
+  redisConfig['password'] = process.env.REDIS_PASSWORD
+}
 
 module.exports = {
-  redis: {
-    host: process.evn.REDIS_HOST,
-    port: 6379,
-  }
+  redis: redisConfig,
 }
