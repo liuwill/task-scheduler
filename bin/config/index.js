607eba2b134c10c23628b1682f59f6a1c3c73c81
liuwill <liuwei_will@qq.com>
2018-06-13 09:59:39 +0800

    add nvmrc for node version

--- bin/config/index.js
+++ bin/config/index.js
@@ -1,10 +1,7 @@
 
 module.exports = {
   redis: {
-    // host: process.evn.REDIS_HOST,
-    // port: 6379,
-    host: 'r-2ze2e2c268e15064.redis.rds.aliyuncs.com',
+    host: process.evn.REDIS_HOST,
     port: 6379,
-    password: 'B2btest981Pls'
   }
 }
